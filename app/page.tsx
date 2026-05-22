d6f7f283fa1fc119cb012bf6cc031b43ad586f75
JUNYEON <bassfriend87@gmail.com>
2026-05-22 13:19:40 +0900

    design: Hero 모바일 이미지 3:4 비율

--- app/page.tsx
+++ app/page.tsx
@@ -128,7 +128,7 @@ export default function HomePage() {
               </div>
             </div>
             {/* 이미지 */}
-            <div className="relative aspect-square md:aspect-auto">
+            <div className="relative aspect-[3/4] md:aspect-auto">
               <Image
                 src="/images/doctor-profile-desk-gray.jpg"
                 alt="김준연 대표원장"
